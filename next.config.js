75c4b6c8e052e81a5ca8f40578a2ea29e1aed240
sungho1103 <galtensh@gmail.com>
2026-02-08 18:00:53 +0900

    Remove static export config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/smart-mbti',
-    images: {
-          unoptimized: true,
-    },
+      images: {
+              unoptimized: true,
+      },
 };
 
 module.exports = nextConfig;
